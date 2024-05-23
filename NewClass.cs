d9c8645eca02ef4299d02c7a1e5a57d0a4d1407d
EvgeniyRob <evgeny.tsyvanin@yandex.ru>
2024-05-23 18:36:43 +0300

    новая ветка

---
+++ NewClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson_23._05_1_
+{
+    internal class NewClass
+    {
+        public NewClass() 
+        {
+            Console.WriteLine("Команда разработчиков 1 !");
+        }
+
+
+    }
+}
