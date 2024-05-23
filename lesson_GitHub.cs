e8c940171759c12a6fe34d2989235914404d09fc
EvgeniyRob <evgeny.tsyvanin@yandex.ru>
2024-05-23 17:54:29 +0300

    Добавьте файлы проекта.

---
+++ lesson_GitHub.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson_23._05_1_
+{
+    internal class lesson_GitHub
+    {
+    }
+}
